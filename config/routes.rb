6b75ed3f7e0b48aae972e83c4b820b56ea01e840
Brian Park <br1anp4rk718@gmail.com>
2016-11-16 17:17:42 -0800

    started on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'user#show'
-
-  
+  root to: 'user#index'
+  resources :surveys
 end
